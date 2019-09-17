4fd11d0142dc4ac949cae5dc3fd6ac15673dde37
RavenRav <akilaia@inbox.ru>
2019-09-17 19:52:10 +0300

    Adapter

--- app/src/main/java/com/raven/raven3hw6/StAdapter.java
+++ app/src/main/java/com/raven/raven3hw6/StAdapter.java
@@ -1,24 +1,37 @@
 package com.raven.raven3hw6;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class StAdapter extends RecyclerView.Adapter<StViewHolder> {
+
+    ArrayList<StudentTask>studentTasks;
+    public StAdapter(){
+        studentTasks = new ArrayList<>();
+
+    }
     @NonNull
     @Override
     public StViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.view_holder_st, parent,false);
+        return new StViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull StViewHolder holder, int position) {
+        holder.onBind(studentTasks.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return studentTasks.size();
     }
 }
