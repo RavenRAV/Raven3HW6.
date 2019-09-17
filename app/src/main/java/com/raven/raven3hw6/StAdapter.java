f609b2968cf878f7003ea056c12e96c854abc9d5
RavenRav <akilaia@inbox.ru>
2019-09-17 19:31:22 +0300

    onBind

--- app/src/main/java/com/raven/raven3hw6/StAdapter.java
+++ app/src/main/java/com/raven/raven3hw6/StAdapter.java
@@ -1,4 +1,24 @@
 package com.raven.raven3hw6;
 
-public class StAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StAdapter extends RecyclerView.Adapter<StViewHolder> {
+    @NonNull
+    @Override
+    public StViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
