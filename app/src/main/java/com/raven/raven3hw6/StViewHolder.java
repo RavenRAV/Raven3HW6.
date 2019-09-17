ca250c4a382aea81de091799a472387ad0950161
RavenRav <akilaia@inbox.ru>
2019-09-17 19:23:57 +0300

    onBind

--- app/src/main/java/com/raven/raven3hw6/StViewHolder.java
+++ app/src/main/java/com/raven/raven3hw6/StViewHolder.java
@@ -1,12 +1,23 @@
 package com.raven.raven3hw6;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class StViewHolder extends RecyclerView.ViewHolder {
+
+    TextView name;
+    TextView infor;
     public StViewHolder(@NonNull View itemView) {
         super(itemView);
+        name = itemView.findViewById(R.id.student_name);
+        infor = itemView.findViewById(R.id.info);
+    }
+
+    public void onBind(StudentTask stT){
+        name.setText(stT.name);
+        infor.setText(stT.inform);
     }
 }
---
+++ app/src/main/java/com/raven/raven3hw6/StudentTask.java
@@ -0,0 +1,11 @@
+package com.raven.raven3hw6;
+
+import java.util.Date;
+
+public class StudentTask {
+    public String name;
+    public String surname;
+    public String inform;
+    public Date birthday;
+
+}
